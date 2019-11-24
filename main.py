1950432a767ae2a7d61826dc3d118c2197ee7d05
Leo Jiménez <leojimenezcr@gmail.com>
2019-11-24 16:11:18 -0600

    Workflow

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import facerecognition
 import signrecognition
 import postprocessing
 
+# Workflow:
+# 1. Video capture -> imgframe
+# 2. imgframe -> facerecoginition -> return emotion label
+# 3. imgframe -> signrecogintion -> return sing label
+# 4. emotion label, sing label -> postprocessing -> return expression
