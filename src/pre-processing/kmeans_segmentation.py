358163d3167223e6167b358959b1efd94f3e9adb
alberto <geovanny@pm.me>
2019-11-25 23:20:35 -0600

    Comments added

--- src/pre-processing/kmeans_segmentation.py
+++ src/pre-processing/kmeans_segmentation.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read the images 
+# read all the images
 image = cv2.imread("skull.jpg")
 
-# convert to RGB
+# convert to RGB, all the images 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # reshape the image to a 2D array of pixels and 3 color values (RGB)
@@ -14,16 +14,18 @@ pixel_values = image.reshape((-1, 3))
 pixel_values = np.float32(pixel_values)
 
 print(pixel_values.shape)
+
 # define stopping criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
-# number of clusters (K)
+# number of clusters (K), define better 
 k = 4
 compactness, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 print(labels)
 print(centers.shape)
 
+# The next part sshouul be commented 
 # convert back to 8 bit values
 centers = np.uint8(centers)
 
