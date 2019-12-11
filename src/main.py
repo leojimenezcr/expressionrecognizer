c97033828813605d2569b50e2b5e591ee167d4c9
alberto <geovanny@pm.me>
2019-12-10 22:40:06 -0600

    Call the sign recognition methods to train and test the net

--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
 
-import cv2
+from signrecognition.signrecognition import run_the_net
 
-import facerecognition.facerecognition
-import signrecognition
-import postprocessing
-
-# Capture a file (path) or webcam (id 0,1,...)
-capture = cv2.VideoCapture(0)
-
-while True:
-    # 1. Video capture -> imgframe
-    ret, imgframe = capture.read()
-
-    # 2. imgframe -> facerecoginition -> return emotion label
-    # 3. imgframe -> signrecogintion -> return sign label
-    # 4. emotion label, sign label -> postprocessing -> return expression
-
-    # Wait for a 'q' key
-    cv2.imshow("Capture (key q for quit): ", imgframe)
-    if cv2.waitKey(10) == ord('q'):
-        break
+if __name__ == "__main__":
+    run_the_net()
+    print('The End!')
