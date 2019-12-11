e95af5e1fe5f77cf25f802af477e986c7916d462
alberto <geovanny@pm.me>
2019-12-10 22:47:42 -0600

    Methos created and code improved

--- src/preprocessing/kmeans_segmentation.py
+++ src/preprocessing/kmeans_segmentation.py
@@ -2,40 +2,34 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read all the images
-image = cv2.imread("skull.jpg")
+def get_image_segmented_image(image, clusters = 5):
+  image = cv2.imread(image)
 
-# convert to RGB, all the images 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  # convert to RGB, all the images
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# reshape the image to a 2D array of pixels and 3 color values (RGB)
-pixel_values = image.reshape((-1, 3))
-# convert to float
-pixel_values = np.float32(pixel_values)
+  # reshape the image to a 2D array of pixels and 3 color values (RGB)
+  pixel_values = image.reshape((-1, 3))
 
-print(pixel_values.shape)
+  # convert to float
+  pixel_values = np.float32(pixel_values)
 
-# define stopping criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+  # define stopping criteria
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
-# number of clusters (K), define better 
-k = 4
-compactness, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+  compactness, labels, (centers) = cv2.kmeans(pixel_values, clusters, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-print(labels)
-print(centers.shape)
+  # convert back to 8 bit values
+  centers = np.uint8(centers)
 
-# The next part should be commented 
-# convert back to 8 bit values
-centers = np.uint8(centers)
+  # convert all pixels to the color of the centroids
+  segmented_image = centers[labels.flatten()]
 
-# convert all pixels to the color of the centroids
-segmented_image = centers[labels.flatten()]
+  # reshape back to the original image dimension
+  segmented_image = segmented_image.reshape(image.shape)
 
-# reshape back to the original image dimension
-segmented_image = segmented_image.reshape(image.shape)
+  return segmented_image
 
-# show the image
-plt.imshow(segmented_image)
-plt.show()
-
+def show_segmented_image(image):
+  plt.imshow(image)
+  plt.show()
